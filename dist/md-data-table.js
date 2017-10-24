f4be33faab34e4e618f2be36747403f3b4eff8b3
CodeFunta <wbr.andrey.l@gmail.com>
2017-10-24 16:07:45 +0300

    fix checkbox click instead of ng-change (since it for model, not DOM change and $event is not accesible)

--- dist/md-data-table.js
+++ dist/md-data-table.js
@@ -931,33 +931,41 @@
         .module('mdDataTable')
         .service('PaginationFeature', PaginationFeature);
 }());
-(function(){
+(function () {
     'use strict';
 
     SelectableRowsFeatureFactory.$inject = ['$timeout'];
-    function SelectableRowsFeatureFactory($timeout){
+    function SelectableRowsFeatureFactory($timeout) {
 
-        function SelectableRowsFeature(params){
+        function SelectableRowsFeature(params) {
             this.$scope = params.$scope;
             this.ctrl = params.ctrl;
 
             this.$scope.onCheckboxChange = _.bind(this.onCheckboxChange, this);
+            this.$scope.onCheckboxClick = _.bind(this.onCheckboxClick, this);
         }
-
-        SelectableRowsFeature.prototype.onCheckboxChange = function($event){
+        SelectableRowsFeature.prototype.onCheckboxClick = function ($event) {
             var that = this;
-            $event.preventDefault();
+            if ($event.stopPropagation) {    // standard
+                $event.stopPropagation();
+            } else {    // IE6-8
+                 $event.cancelBubble = true;
+            }
+        };
+        SelectableRowsFeature.prototype.onCheckboxChange = function () {
+            var that = this;
+
             // we need to push it to the event loop to make it happen last
             // (e.g.: all the elements can be selected before we call the callback)
-            $timeout(function(){
+            $timeout(function () {
                 that.$scope.selectedRowCallback({
                     rows: that.ctrl.dataStorage.getSelectedRows()
                 });
-            },0);
+            }, 0);
         };
 
         return {
-            getInstance: function(params){
+            getInstance: function (params) {
                 return new SelectableRowsFeature(params);
             }
         };
